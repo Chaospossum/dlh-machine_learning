6d8bbf7682fd901e897ccca79ce1683c798e9acc
Chaospossum <duqni042@gmail.com>
2026-05-18 22:49:01 +0200

    exercice duh

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that provides a function to determine the shape of a matrix."""
+def matrix_shape(matrix):
+    """Return the shape of a matrix as a list of integers."""
+
+    shape = []
+    while isinstance(matrix,list):
+
+        shape.append(len(matrix))
+        matrix = [0]matrix
+    return shape
