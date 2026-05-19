1e9b80002a614e1cfcf740fb0decb532fc3e6b4c
Chaospossum <duqni042@gmail.com>
2026-05-19 20:39:49 +0200

    exercice duh

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that provides a function to transpose a 2D matrix """
+
+def matrix_transpose(matrix):
+    """Return the transpose of a 2D matrix"""
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+
+    transposed = []
+    for i in range(num_cols)
+        new_row = []
+        for j in(num_rows)
+            new_row.append(matrix[j][i])
