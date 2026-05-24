89d6b59790be6448fe635f82d16b78f14895b647
Chaospossum <duqni042@gmail.com>
2026-05-24 23:54:08 +0200

    PYCODESTYLE

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,5 +4,5 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """Return a new numpy.ndarray from concatenating mat1 and mat2 along axis."""
+    """Return a new numpy.ndarray from concatenating along axis."""
     return np.concatenate((mat1, mat2), axis=axis)
