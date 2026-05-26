a3cf172031708f6159e8a77b1e4af2aaed2c005a
Chaospossum <duqni042@gmail.com>
2026-05-26 16:26:56 +0200

    pray to the machine gods

--- pipeline/databases/1-first_table.sql
+++ pipeline/databases/1-first_table.sql
@@ -1,5 +1,5 @@
---Create table first_table if it does not exist
-CREATE TABLE FIRST_TABLE IF IT NOT EXIST (
-    id INT
+-- Create table first_table if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
     name VARCHAR(256)
 );
