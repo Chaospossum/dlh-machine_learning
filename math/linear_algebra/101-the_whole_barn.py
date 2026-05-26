6ca886611cdc156d981f26017b320bd078686451
Chaospossum <duqni042@gmail.com>
2026-05-26 14:11:34 +0200

    pray to the machine gods

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from numpy import matrix
 
 
 def add_matrices(mat1, mat2):
@@ -6,3 +7,4 @@ def add_matrices(mat1, mat2):
         return None
     if not isinstance(mat1,list):
         return mat1 + mat2
+    return [add_matrices(mat1[i],mat2[i]) for i in range[len(mat)]]
