f9031c09b2fa37fff4b8f3b99cb27f061b599b58
Chaospossum <duqni042@gmail.com>
2026-05-26 14:17:56 +0200

    pray to the machine gods

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
-from numpy import matrix
+"""Module that provides a function to add two matrices."""
+
+
+def matrix_shape(matrix):
+    """Return the shape of a matrix as a list of integers."""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
 
 
 def add_matrices(mat1, mat2):
-    if matrix.shape(mat1) != matrix.shape(mat2):
+    """Return a new matrix with element-wise sums, or None if shapes differ."""
+    if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    if not isinstance(mat1,list):
+
+    if not isinstance(mat1, list):
         return mat1 + mat2
-    return [add_matrices(mat1[i],mat2[i]) for i in range[len(mat)]]
+
+    return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
