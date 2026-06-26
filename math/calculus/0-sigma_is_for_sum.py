b3f41ea9be7fc81101dbddc43017cac1bc605700
Chaospossum <duqni042@gmail.com>
2026-06-27 00:53:01 +0200

    Remove .py version of multiple-choice answer file

--- math/calculus/0-sigma_is_for_sum.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-"""Sigma is for Sum: evaluate the summation of i for i = 2 to 5."""
-total = sum(range(2, 6))
-choices = {1: 3 + 4 + 5, 2: 3 + 4, 3: 2 + 3 + 4 + 5, 4: 2 + 3 + 4}
-print(next(number for number, value in choices.items() if value == total))
