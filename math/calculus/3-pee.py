1d7c648aae5414022e1dbc49834b252dad2a268a
Chaospossum <duqni042@gmail.com>
2026-06-27 00:53:08 +0200

    Remove .py version of multiple-choice answer file

--- math/calculus/3-pee.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""The Greeks pronounce it pEE: evaluate the product of i for i = 0 to 10."""
-product = 1
-for i in range(0, 11):
-    product *= i
-# The factor i = 0 makes the whole product 0, which is choice 4.
-print(4 if product == 0 else None)
