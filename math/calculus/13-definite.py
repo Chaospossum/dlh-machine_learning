5cc25fa422443543f9b18e58efa767b68d7030c7
Chaospossum <duqni042@gmail.com>
2026-06-27 00:53:55 +0200

    Remove .py version of multiple-choice answer file

--- math/calculus/13-definite.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Create a definite plan...: definite integral of x^2 from 0 to 3."""
-import sympy
-
-x = sympy.Symbol('x')
-value = sympy.integrate(x**2, (x, 0, 3))
-choices = {1: 3, 2: 6, 3: 9, 4: 27}
-print(next(n for n, v in choices.items() if v == value))
