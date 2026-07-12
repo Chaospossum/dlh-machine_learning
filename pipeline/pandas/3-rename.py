240003cb452a3367c2393e02bbd740dda8032fa9
Chaospossum <duqni042@gmail.com>
2026-07-12 23:03:51 +0200

    .

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+
+def rename(df):
+    """Rename Timestamp to Datetime, convert to datetime, and select columns."""
+    df = df.rename(columns={'Timestamp': 'Datetime'})
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    return df[['Datetime', 'Close']]
